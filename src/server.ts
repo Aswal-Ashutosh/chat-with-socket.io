586d5e66440fc30b393ceadf05c277e90095807f
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2022-12-20 13:01:37 +0530

    create simple chatting application using socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import http from "http";
+import { Server } from "socket.io";
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log(`User[${socket.id}] Connected!`);
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    });
+});
 
 server.listen(3000, () => {
     console.log("Running🏃.");
