2e29ef7e6061cd3c45976856c23743569d2c3dc2
Aswal-Ashutosh <ashu.aswal.333@gmail.com>
2022-12-22 16:59:11 +0530

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
-import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.sendFile(__dirname + "/index.html");
 });
+
+const server = app.listen(3000, () => {
+    console.log("Running🏃.");
+});
+
+const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST", "PUT", "PATCH", "DELETE"] } });
 
 io.on("connection", (socket) => {
     console.log(`User[${socket.id}] Connected!`);
     socket.on("chat message", (msg) => {
         io.emit("chat message", msg);
     });
-});
-
-server.listen(3000, () => {
-    console.log("Running🏃.");
 });
